90885c5dba91345e1de4a5c93f5a6d21a9ebbca0
Matt Carter <m@ttcarter.com>
2021-08-29 13:59:53 +1000

    feat: Basic prorotype now working with tests

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+var debug = require('debug')('express-jail');
+var exec = require('@momsfriendlydevco/exec');
+var eventer = require('@momsfriendlydevco/eventer');
+
 /**
 * Actual express middleware layer
 * @param {Object} [options] Options to adjust behaviour, `expressJail.defaults` is used to populate this
 * @returns {ExpressMiddleware} Express compatible middleware function
+*
+* @emits ban Emitted as `({ip})` before an IP is banned, if the async return is boolean `false` the adding operation is aborted
+* @emits banned Emitted as `({ip})` after an IP is banned
+*
+* @emits ban Emitted as `({ip})` before an IP is unbanned, if the async return is boolean `false` the removal operation is aborted
+* @emits banned Emitted as `({ip})` after an IP is unbanned
 */
 var expressJail = module.exports = function expressJailMiddleware(options) {
 	var settings = {
@@ -12,14 +22,133 @@ var expressJail = module.exports = function expressJailMiddleware(options) {
 	// Convert settings.paths into a Set for easier querying / dedupeing
 	settings.paths = new Set(settings.paths);
 
-	return function(req, res, next) {
-		if (settings.paths.has(req.path)) {
-			console.log('DISALLOW FORBIDDEN PATH', req.path);
-			res.sendStatus(settings.responseCode);
-		} else {
-			next();
-		}
+	// Boot & prepare jail
+	var bootComplete = false;
+	var bootPromise = Promise.resolve()
+		// Try pinging server {{{
+		.then(()=> exec([...settings.clientBinary, 'ping'], {buffer: true})
+			.catch(e => { throw new Error(`F2B-client ping error: ${e.toString()}`) })
+			.then(res => res != 'Server replied: pong' && Promise.reject(`Unexpected response when pinging F2B - ${res}`))
+		)
+		// }}}
+		// Query existing jails list {{{
+		.then(()=> exec([...settings.clientBinary, 'status'], {buffer: true})
+			.catch(e => { throw new Error(`F2B-client status error: ${e.toString()}`) })
+		)
+		// }}}
+		// Check jail exists or create it {{{
+		.then(res => {
+			var f2bRes = /\- Jail list:\s*(?<jails>.*)/ms.exec(res)?.groups;
+			f2bRes = {
+				jails: f2bRes?.jails
+					? f2bRes.jails.split(/\s*,\s*/)
+					: []
+			};
+
+			if (f2bRes.jails.includes(settings.jail)) {
+				debug(`F2B jail "${settings.jail}" already exists - skipping creation`);
+			} else {
+				debug(`F2B jail "${settings.jail}" doesnt exist - creating`);
+				return exec([...settings.clientBinary, 'add', settings.jail])
+					.catch(e => { throw new Error(`F2B-client jail creation error: ${e.toString()}`) })
+			}
+		})
+		// }}}
+		// End {{{
+		.then(()=> bootComplete = true)
+		.catch(e => console.warn('express-jail - setup error', e))
+		// }}}
+
+	var jailMiddleware = function(req, res, next) {
+		Promise.resolve()
+			.then(()=> !bootComplete && bootPromise)
+			.then(()=> {
+				if (settings.paths.has(req.path)) {
+					return jailMiddleware.ban(req.ip, req)
+						.then(()=> res.sendStatus(settings.responseCode))
+				} else {
+					next();
+				}
+			})
 	};
+
+
+	/**
+	* Ban an incomming IP address by adding it to the F2B jail
+	* @param {string} ip The IP address to ban
+	* @returns {Promise} A promise which resolves when the operation has completed
+	*
+	*/
+	jailMiddleware.ban = function expressJailBan(ip, req) {
+		return Promise.resolve()
+			.then(()=> jailMiddleware.emit('ban', {ip: ip, req}))
+			.then(doBan => { if (doBan === false) throw 'SKIP' })
+			.then(()=> exec([...settings.clientBinary, 'set', settings.jail, 'banip', ip], {buffer: true})
+				.catch(e => { throw new Error(`F2B-client add-to-jail error: ${e.toString()}`) })
+			)
+			.then(()=> jailMiddleware.emit('banned', {ip: ip, req}))
+			.catch(e => {
+				if (e === 'SKIP') return;
+				throw e;
+			})
+	}
+
+
+	/**
+	* Unban an incomming IP address by removing it from the F2B jail
+	* @param {string} ip The IP address to unban
+	* @returns {Promise} A promise which resolves when the operation has completed
+	*
+	*/
+	jailMiddleware.unban = function expressJailUnban(ip, req) {
+		return Promise.resolve()
+			.then(()=> jailMiddleware.emit('unban', {ip: ip, req}))
+			.then(doBan => { if (doBan === false) throw 'SKIP' })
+			.then(()=> exec([...settings.clientBinary, 'set', settings.jail, 'unbanip', ip], {buffer: true})
+				.catch(e => { throw new Error(`F2B-client remove-from-jail error: ${e.toString()}`) })
+			)
+			.then(()=> jailMiddleware.emit('unbanned', {ip: ip, req}))
+			.catch(e => {
+				if (e === 'SKIP') return;
+				throw e;
+			})
+	}
+
+
+	/**
+	* Return all bans in the jail
+	*/
+	jailMiddleware.bans = function expressJailBans(ip) {
+		return Promise.resolve()
+			.then(()=> exec([...settings.clientBinary, 'get', settings.jail, 'banip', '--with-time'], {buffer: true})
+				.catch(e => { throw new Error(`F2B-client query-jail error: ${e.toString()}`) })
+			)
+			.then(bans => bans.split(/\n/)
+				.map(line => /^(?<ip>[\d\.]+)\s+(?<from>[\d\-\s:]+) \+ (?<time>\d+) = (?<to>[\d\-\s:]+)$/.exec(line)?.groups)
+				.filter(Boolean)
+				.map(ban => ({
+					ip: ban.ip,
+					from: new Date(ban.from),
+					time: parseInt(ban.time),
+					to: new Date(ban.to),
+				}))
+			)
+	};
+
+
+	/**
+	* Query if a given IP exists within the jail
+	* This is really just a lazy bans() + filter convenience function
+	* @param {string} ip The IP to query for
+	*/
+	jailMiddleware.hasBan = function expressJailHasBan(ip) {
+		return jailMiddleware.bans()
+			.then(bans => bans.some(ban => ban.ip == ip))
+	};
+
+
+	eventer.extend(jailMiddleware);
+	return jailMiddleware;
 };
 
 expressJail.defaults= {
@@ -40,4 +169,7 @@ expressJail.defaults= {
 		'/ecp/Current/exporttool/microsoft.exchange.ediscovery.exporttool.application',
 	],
 	responseCode: 404,
+
+	clientBinary: ['/usr/bin/sudo', '/usr/bin/fail2ban-client'],
+	jail: 'www',
 };
--- test/test.js
+++ test/test.js
@@ -6,17 +6,21 @@ var expressLogger = require('express-log-url');
 
 var port = 8181;
 var url = `http://localhost:${port}`;
+var thisIp = '127.0.0.1'; // This IP address, used when resolving if this client was banned
 
-describe('express-fail2ban', ()=> {
+describe('express-jail', ()=> {
 
-	var app, server;
-	before('setup dummy server', finish => {
+	var app, jail, server;
+	before('setup server', finish => {
 		app = express();
 		app.use(expressLogger);
 		app.set('log.indent', '      ');
-		app.use(expressJail({
+
+		jail = expressJail({
 			responseCode: 403,
-		}));
+		})
+		app.use(jail);
+
 		app.use('/api/foo', (req, res) => res.send({string:'Foo!'}));
 		server = app.listen(port, null, finish);
 	});
@@ -28,12 +32,78 @@ describe('express-fail2ban', ()=> {
 			.then(({data}) => expect(data).be.deep.equal({string:'Foo!'}))
 	)
 
-	it('should detect hits to forbidden paths', ()=>
-		axios.get(`${url}/.git/HEAD`)
+	it('should detect hits to forbidden paths', ()=> {
+		var detectedBan = false;
+		jail.once('ban', info => detectedBan = true);
+
+		return axios.get(`${url}/.git/HEAD`)
 			.then(expect.fail)
 			.catch(e => {
 				expect(e).to.have.nested.property('response.status', 403);
+				expect(detectedBan).to.be.equal(true);
 			})
-	)
+	})
+
+	it('should get the current ban list', ()=>
+		jail.bans()
+			.then(bans => {
+				expect(bans).to.be.an('array');
+				bans.forEach(ban => {
+					expect(ban).to.have.property('ip');
+					expect(ban.ip).to.be.a('string');
+
+					expect(ban).to.have.property('from');
+					expect(ban.from).to.be.a('date');
+
+					expect(ban).to.have.property('time');
+					expect(ban.time).to.be.a('number');
+
+					expect(ban).to.have.property('to');
+					expect(ban.to).to.be.a('date');
+				});
+			})
+	);
+
+	it('should have this IP on the ban list - via jail.bans()', ()=>
+		jail.bans()
+			.then(bans => {
+				expect(bans.some(b => b.ip == thisIp)).to.be.equal(true);
+			})
+	);
+
+	it('should have this IP on the ban list - via jail.hasBan(ip)', ()=>
+		jail.hasBan(thisIp)
+			.then(hasBan => {
+				expect(hasBan).to.equal(true)
+			})
+	);
+
+	// NOTE: This is skipped because the method F2B uses to actually ban IP's doesn't work with localhost
+	it.skip('subsequent access should timeout', ()=>
+		axios.get(`${url}/.git/HEAD`, {timeout: 500})
+			.then(expect.fail)
+			.catch(e => {
+				console.log('TIMEOUT', e);
+				expect(e).to.not.have.nested.property('response.status', 403);
+			})
+	);
+
+	it('should remove the IP from the ban list', ()=>
+		jail.unban(thisIp)
+	);
+
+	it('should have NOT have this IP on the ban list - via jail.bans()', ()=>
+		jail.bans()
+			.then(bans => {
+				expect(bans.some(b => b.ip == thisIp)).to.be.equal(false);
+			})
+	);
+
+	it('should have NOT have this IP on the ban list - via jail.hasBan(ip)', ()=>
+		jail.hasBan(thisIp)
+			.then(hasBan => {
+				expect(hasBan).to.equal(false)
+			})
+	);
 
 });
